c7a93d0a0cc333d6eb6b10f5359a5a15b140757c
muqeem <muqeem012@gmail.com>
2024-05-17 22:35:47 +0500

    Created Yaml Files for all 5 Episodes

--- episode-05/backend/src/server.js
+++ episode-05/backend/src/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 try {
-	mongoose.connect(process.env.MONGO_DB_SECRET, {
+	mongoose.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
